5b899fbbbd250f46e36af7851755bbb335695d9d
tahmidtaqi <taqitahmid97@gmail.com>
2019-04-14 22:29:06 +0600

    About_fix

--- app/src/main/java/com/codexaxor/mac/googlevisionv1/About.java
+++ app/src/main/java/com/codexaxor/mac/googlevisionv1/About.java
@@ -2,6 +2,8 @@ package com.codexaxor.mac.googlevisionv1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+
 
 public class About extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+
     }
 }
